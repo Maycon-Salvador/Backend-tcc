b4ae54cfb865f32d16126cb126f786682956e6fb
Maycon Salvador <maycon.salvadev@gmail.com>
2025-04-29 18:54:14 -0400

    usando config para conectar com PostgreSQL no Render

--- MedAgenda/settings.py
+++ MedAgenda/settings.py
@@ -86,13 +86,14 @@ WSGI_APPLICATION = 'MedAgenda.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'medagenda_db',
-        'USER': 'postgres',
-        'PASSWORD': '23032024',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
+
 
 
 # Password validation
